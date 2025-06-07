bc1db4924047f01eea69d60d4e5a612a43e2bb4f
Elie MANIZABAYO <manizabayoelie.com>
2025-06-07 12:32:09 +0200

    feat: add footer components

---
+++ my-app/src/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+ function Footer() {
+  return (
+    <div>
+        <p>&copy; {new Date().getFullYear()} Eternity Company Limited</p>
+    </div>
+  )
+}
+export default  Footer
