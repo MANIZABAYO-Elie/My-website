df3ef9e23fe776179cd79322e7535a425f3c814e
Elie MANIZABAYO <manizabayoelie.com>
2025-06-08 11:28:40 +0200

    feat: add card component

---
+++ my-app/src/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import target from './assets/target.jpg'
+function Card() {
+  return (
+    <div className='card'>
+        <img src={target} alt="frontend developer" />
+        <h2>MANIZABAYO Elie</h2>
+        <p>Frontend Developer</p>
+    </div>
+  )
+}
+export default  Card
